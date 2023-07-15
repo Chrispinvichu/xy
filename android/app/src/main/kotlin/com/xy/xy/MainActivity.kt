cd5dc8756c3a92ef0728be8665f7c048888e1763
Chrispin <thangzzz2828@gmail.com>
2023-07-15 15:02:53 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/xy/xy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.xy.xy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Splash_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+
+class SplashApp extends StatelessWidget {
+  const SplashApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
